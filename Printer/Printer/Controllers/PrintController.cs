69df02aab35d0974ae8905f9a220686b9522bb99
Aizaz Ahmed <aizaz.ahmed@brightfuture.co.uk>
2015-07-15 13:13:19 +0100

    It send the print command and peint static page

---
+++ Printer/Printer/Controllers/PrintController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Printer.Controllers
+{
+    using Neodynamic.SDK.Web;
+    using Properties;
+
+    public class PrintController : Controller
+    {
+        // GET: Print
+        public ActionResult Print()
+        {
+            return View();
+        }
+
+        public void PrintInvoice()
+        {
+            var printJob = new ClientPrintJob();
+            printJob.ClientPrinter= new DefaultPrinter();
+
+            printJob.PrinterCommands = Resources.PrinterText;
+
+            printJob.SendToClient(System.Web.HttpContext.Current.Response);
+        }
+    }
+}
